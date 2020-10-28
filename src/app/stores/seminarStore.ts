6a4de4ce4655d60df27227a682d4e6d5b653db31
Rimon89 <rimon.fayad@gmail.com>
2020-10-28 11:14:16 +0100

    removed strict-mode

--- src/app/stores/seminarStore.ts
+++ src/app/stores/seminarStore.ts
@@ -1,43 +1,49 @@
-import { observable, action, computed, configure, runInAction } from 'mobx';
+import { observable, action, computed } from 'mobx';
 import { createContext } from 'react';
 import { toast } from 'react-toastify';
 import agent from '../api/agent';
 import { ISeminar } from '../models/seminar';
 import { IUser } from '../models/user';
 
-configure({ enforceActions: 'always' });
-
 export class SeminarStore {
-
   @observable seminarRegistry = new Map();
   @observable seminar: ISeminar | null = null;
   @observable loadingInitial = false;
-  @observable searchByDateOrName: Date | Date[] | string = 'all'
+  @observable searchByDateOrName: Date | Date[] | string = 'all';
 
   @action setSearchByDateOrName = (value: Date | Date[] | string) => {
     this.searchByDateOrName = value;
-  }
+  };
 
   @computed get seminarsByDateOrName() {
-    if(this.searchByDateOrName === 'all') {
-      return Array.from(this.seminarRegistry.values()).filter(seminar => seminar.availableSeats > 0)
+    if (this.searchByDateOrName === 'all') {
+      return Array.from(this.seminarRegistry.values()).filter(
+        (seminar) => seminar.availableSeats > 0
+      );
     }
-    const formatDate = this.searchByDateOrName.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric'})
-    return Array.from(this.seminarRegistry.values()).filter(seminar => seminar.availableSeats > 0)
-          .filter(date => date.dateTime.split('T')[0] === formatDate
-           || date.name.toLowerCase().includes(this.searchByDateOrName)
-           || date.category.toLowerCase() === this.searchByDateOrName);
+    const formatDate = this.searchByDateOrName.toLocaleString([], {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    });
+    return Array.from(this.seminarRegistry.values())
+      .filter((seminar) => seminar.availableSeats > 0)
+      .filter(
+        (seminar) =>
+        seminar.dateTime.split('T')[0] === formatDate ||
+        seminar.name.toLowerCase().includes(this.searchByDateOrName) ||
+        seminar.category.toLowerCase() === this.searchByDateOrName
+      );
   }
 
   @action loadSeminars = async () => {
     this.loadingInitial = true;
     try {
       const seminars = await agent.Seminars.list();
-      runInAction('loading seminars', () => {
-        seminars.forEach((seminar) => {
-          this.seminarRegistry.set(seminar.id, seminar);
-          this.loadingInitial = false;
-        });
+
+      seminars.forEach((seminar) => {
+        this.seminarRegistry.set(seminar.id, seminar);
+        this.loadingInitial = false;
       });
     } catch (error) {
       console.log(error);
@@ -47,25 +53,19 @@ export class SeminarStore {
   @action loadSeminar = async (id: string) => {
     this.loadingInitial = true;
     try {
-      let seminar = await agent.Seminars.details(id)
-      runInAction('getting seminar', () => {
-        this.seminar = seminar;
-        this.loadingInitial = false;
-      })
+      let seminar = await agent.Seminars.details(id);
+      this.seminar = seminar;
+      this.loadingInitial = false;
     } catch (error) {
-      runInAction('get seminar error', () => {
-        this.loadingInitial = false;
-        console.log(error)
-      })
+      this.loadingInitial = false;
+      console.log(error);
     }
-  } 
+  };
 
   @action createUser = async (user: IUser) => {
     try {
       await agent.Users.create(user);
-      runInAction('creating attendee', () => {
-        toast.success('Thank you for choosing JoyEducation.');
-      });
+      toast.success('Thank you for choosing JoyEducation.');
     } catch (error) {
       console.log(error.response);
     }
